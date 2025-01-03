12d4ea1e46dfe30eacce89e7dbd971d47e75b1e9
남서현 <syn5421a@gmail.com>
2025-01-03 15:48:50 +0900

    CRUD 레이아웃

--- eommpj/shop/models.py
+++ eommpj/shop/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 # Create your models here.
 class Blog(models.Model): #글게시판 모델
     title = models.CharField(max_length=100)
-    #author = models.CharField(max_length=50)
+    # author = models.CharField(max_length=50)
     #head_image = models.ImageField(upload_to='shop/images/%y/%m/%d/', blank=True)
     #file_upload = models.FileField(upload_to='shop/Files/%y/%m/%d/', blank=True)
     content = models.TextField()
