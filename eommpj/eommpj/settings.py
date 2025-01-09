bad215dbea0be4006f08a0d481ad5d80bc1fd038
Tae Young <49816869+teon98@users.noreply.github.com>
2025-01-10 00:21:03 +0900

    render disk 추가

--- eommpj/eommpj/settings.py
+++ eommpj/eommpj/settings.py
@@ -86,8 +86,8 @@ STATICFILES_DIRS = [
 # 업로드된 파일의 URL 경로
 MEDIA_URL = '/media/'
 # 업로드된 파일의 실제 저장 위치
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/media/'
 # 압축된 정적 파일 제공 설정
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
